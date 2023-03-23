12a0eb4971634ca69f147479ed1bd5ef4b900aba
juliock <juliock@tecgraf.puc-rio.br>
2023-03-23 14:17:50 -0300

    first commit

---
+++ springboot-restfull-api/src/main/java/br/com/julio/springbootrestfullapi/SpringbootRestfullApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.julio.springbootrestfullapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestfullApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestfullApiApplication.class, args);
+	}
+
+}
---
+++ springboot-restfull-api/src/test/java/br/com/julio/springbootrestfullapi/SpringbootRestfullApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.julio.springbootrestfullapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestfullApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
